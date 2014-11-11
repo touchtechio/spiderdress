59d7db30b7594a84519a567013e3ea5d4c1ca066
MatthewX Pinner <matthewx.pinner@intel.com>
2014-11-11 13:36:11 -0800

    added hostap option and comments

--- bringup/root/init.sh
+++ bringup/root/init.sh
@@ -2,8 +2,17 @@
 
 main() {
     #cleanup_processes
+    
+    #start edison as access point
+    #systemctl start hostapd
+
+    # setting up to connect to another hotspot
     #systemctl start wpa_supplicant
+    
+    #setting up bluetooth
     #rfkill unblock bluetooth
+    
+    # else where??
     gpio_init_i2c6
     gpio_init_uart
 }
