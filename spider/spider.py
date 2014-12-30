140a8d3f4924c944be20c2a3c5720e9e52aa5ed3
Jason Blanchard <jason.blanchard@intel.com>
2014-12-29 20:38:39 -0800

    Fixes bug in button press code, allowing modes to change correctly.
    Signed-off-by: Jason Blanchard <jason.blanchard@intel.com>

--- spider/spider.py
+++ spider/spider.py
@@ -1,4 +1,5 @@
 import sys
+from multiprocessing import Value
 
 import maestro_controller
 import respiration
@@ -15,12 +16,26 @@ class Spider(object):
 
         self.maestro = maestro_controller.MaestroController(leg_file)
         self.proxemic = proximity.Proxemic(proximity.Proximity.DEFAULT_CHANNELS)
-        self.respiration = respiration.Respiration()
-        self.button = button.Button()
+        #self.respiration = respiration.Respiration()
+        self.button = button.Button(channel=1)
+        self.start_mode_flag = Value('b', False, lock=True)
 
     def start(self):
         self.button.monitor_button(self._button_listener)
         self._start_mode()
+
+        while True:
+            if self.start_mode_flag.value:
+                self.start_mode_flag.value = False
+
+                self._stop_mode()
+
+                if self.mode == CES_MODE:
+                    self.mode = SLOW_BREATHE_MODE
+                else:
+                    self.mode = CES_MODE
+
+                self._start_mode()
 
     def stop(self):
         self._stop_mode()
@@ -30,28 +45,20 @@ class Spider(object):
             self.maestro.start_ces_animation()
             self.proxemic.monitor_space(
                 self.maestro.prox_space_listener, self.maestro.prox_distance_listener)
-            self.respiration.monitor_respiration(self.maestro.respiration_listener)
+            #self.respiration.monitor_respiration(self.maestro.respiration_listener)
         else:
             self.maestro.start_slow_breathe_mode()
 
     def _stop_mode(self):
         if self.mode == CES_MODE:
             self.maestro.stop_ces_animation()
-            self.respiration.stop_monitor()
+            #self.respiration.stop_monitor()
         else:
             self.maestro.stop_slow_breathe_mode()
 
     def _button_listener(self):
-        if self.mode == CES_MODE:
-            self._stop_mode()
-
-            self.mode = SLOW_BREATHE_MODE
-            self._start_mode()
-        else:
-            self._stop_mode()
-
-            self.mode = CES_MODE
-            self._start_mode()
+        print "BUTTON PRESS"
+        self.start_mode_flag.value = True
 
         return True
 
